eec5e2028bffa6e7cbdfa72dec46f2c1c8678f6c
jamietopholm-creator <jamietopholm@gmail.com>
2025-08-25 23:29:26 +0200

    Update Obelisk.lua

--- Obelisk.lua
+++ Obelisk.lua
@@ -72,6 +72,22 @@ G_Left:AddButton({
     end,
 })
 
+
+     -- One-click button: Sorcerer Obelisk upgrade (same style as others)
+G_Left:AddButton({
+    Text = 'Activate Sorcerer Obelisk',
+    Func = function()
+        local args = {
+            {
+                Upgrading_Name = "Obelisk",
+                Action = "_Upgrades",
+                Upgrade_Name = "Sorcerer_Obelisk",
+            },
+        }
+        Script.ToServer:FireServer(unpack(args))
+    end,
+})
+
      
     -- ---- Auto Upgrade section ----
     local Upgrade_Group = ObeliskTab:AddLeftGroupbox('Auto Upgrade', 'trending-up')
